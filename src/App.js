ef5e6f63534de9255f830215da5dfb341e2226d9
Kushagra Soni <soni.kushagra24@gmail.com>
2022-08-24 23:50:19 -0400

    Updated Bison Logo and Titles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import bisonLogo from './assets/bison-logo.png';
+import bisonText from './assets/bison-text.png';
+import fitZoneText from './assets/fitness-zone-text.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={bisonLogo} className="App-logo" alt="bisonLogo"/>
+                <img src={bisonText} className="App-logo" alt="bisonText"/>
+                <img src={fitZoneText} className="App-logo" alt="logo"/>
+                <p>
+                    Bison Fitness Zone Website !!
+                </p>
+                <p>
+                    COMING SOON !!
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
